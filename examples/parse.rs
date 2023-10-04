53a15c9f7922e8d59d48752820ab8d51f75023d7
Gregor Purdy <gregor@abcelo.com>
2023-10-04 04:43:01 -0700

    Remove unneeded to_string() call on LEI (it already implements Display)

--- examples/parse.rs
+++ examples/parse.rs
@@ -2,7 +2,7 @@ fn main() {
     let lei_string = "YZ83GD8L7GG84979J516"; // Example from Section A.1 of The Standard
     match lei::parse(lei_string) {
         Ok(lei) => {
-            println!("Parsed LEI: {}", lei.to_string()); // "YZ83GD8L7GG84979J516"
+            println!("Parsed LEI: {}", lei); // "YZ83GD8L7GG84979J516"
             println!("  LOU ID: {}", lei.lou_id()); // "YZ83"
             println!("  Entity ID: {}", lei.entity_id()); // "GD8L7GG84979J5"
             println!("  Check digits: {}", lei.check_digits()); // "16"
